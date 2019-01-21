c876dfe7f8516355d9023d2c9804093c1401d1c2
grantfogle <grantjfogle@gmail.com>
2019-01-21 15:05:28 -0700

    multi join statement set up and operational

--- app.js
+++ app.js
@@ -11,4 +11,8 @@ app.get('/', (req, res) => {
     queries.listAllWildflowers().then(wildflowers => res.send(wildflowers))
 })
 
+app.get('/users/:id', (req, res) => {
+    queries.getWildflowersByUser(req.params.id).then(usersflowers => res.send(usersflowers))
+})
+
 app.listen(port, console.log(`Server running on ${port}`));
--- queries.js
+++ queries.js
@@ -4,7 +4,11 @@ module.exports = {
     listAllWildflowers() {
         return database('wildflowers');
     },
-    getWildflowersByUser() {
-
+    getWildflowersByUser(id) {
+        return database('userstoflowers')
+            .select('wildflowers.name', 'wildflowers.family', 'wildflowers.description', 'wildflowers.image')
+            .join('wildflowers', 'wildflowers.id', 'userstoflowers.wildflower_id')
+            .join('users', 'users.user_id', 'userstoflowers.user_id')
+            .where('users.user_id', id)
     }
 }
