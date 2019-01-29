4763605338fc540d1dd469bd4f760095cec698f1
grantfogle <grantjfogle@gmail.com>
2019-01-29 11:01:56 -0700

    updating seed data columbine and bush cinquefoil

--- seeds/01_wildflowers.js
+++ seeds/01_wildflowers.js
@@ -8,13 +8,13 @@ exports.seed = function (knex, Promise) {
         {
           name: 'Columbine',
           family: 'Buttercup Family',
-          description: 'Colorado\'s queenly state flower speaks for itself. The flower if formed of 5 sepals and 5 petals, alternatively arranded all of them showy.',
+          description: 'Colorado\'s queenly state flower speaks for itself. Composed of 5 sepals and 5 petals, alternately arranged and all of them showy. The sepals are deep blue or sometimes quite pale forming a wide saucer-like star 3 inches across. Plants are 2 feet or more high of several delicate stems, usually carrying at their tops numerous flowers. Grows in rich soil in montane zone, but extends into foothills and up to timberline. Blooms June-July.',
           image: 'https://s22409.pcdn.co/wp-content/uploads/2014/06/A1.jpg'
         },
         {
           name: 'Bush Cinquefoil',
           family: 'Rose Family',
-          description: 'This thornless yellow rose is one of the most widespread and most ornamental shrubs of the mountain states. Flowers are an inch in diamter, 5 broad golden petals surrounding 20 or more stamens.',
+          description: 'This thornless yellow rose is one of the most widespread and most ornamental shrubs of the mountain states. Individual clumps are rarely fully covered with bloom at any one time. Flowers are an inch in diameter, composed of 5 broad, golden petals surrounding 20 or more stamesns. Plant is a dense shrub about 3 to 4 feet high with many dark, wood, feely-branching stems. Grows in moist parts of the montae zone, also in the upper foothills and the lower sub-alpine zones. Blooms continuously May to September.',
           image: 'https://ak8.picdn.net/shutterstock/videos/1008584278/thumb/1.jpg'
         },
         {
